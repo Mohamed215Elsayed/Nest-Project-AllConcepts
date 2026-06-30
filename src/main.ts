bb343397d1e8d42c3096d4b68db5338d85e442d1
MohamedElsayed <101253953+Mohamed215Elsayed@users.noreply.github.com>
2026-06-30 14:27:43 +0300

    docs: add initial project setup documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+// import { NestExpressApplication } from '@nestjs/platform-express';
+// The entry file of the application which uses the core function NestFactory to create a Nest application instance.
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { abortOnError: false });
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule, {abortOnError: false});
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+// bootstrap();
+// void bootstrap();
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
